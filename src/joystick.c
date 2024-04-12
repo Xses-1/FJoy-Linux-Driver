1605b025b51fcf0ff53b63e7eb59a92a185f2a07
xses <witort52@gmail.com>
2024-04-12 13:29:08 +0200

    sync back

--- src/joystick.c
+++ src/joystick.c
@@ -4,16 +4,35 @@
 #include <linux/joystick.h>
 
 int main(void) {
-	int joystick;
+	int in_joy_1, uinput;
 	struct js_event event;
 	struct axis_state axes[4] = {0};
 	size_t axis;
 
 	/**** This has to be changed for the autodetection and auto pairing ****/
-	joystick = open("/dev/input/js1", O_RDONLY);
+
+	/* Opening the inputs */
+	in_joy_1 = open("/dev/input/js1", O_RDONLY);
 
 	if (joystick == -1) {
-		perror("Could not open the joystick!");
+		perror("Could not open the input joystick 1!");
+		return -1;
 	}
 
-	/* This exits when the controller is upnplugged 
+	/* Opening the output */
+	uinput = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+
+	if (uinput == -1) {
+		perror("Could not open the output file!");
+		return -1;
+	}
+
+	ioctl(fd, UI_SET_EVBIT, EV_KEY); // enable button/key handling
+	ioctl(fd, UI_SET_KEYBIT, BTN_A);
+	ioctl(fd, UI_SET_KEYBIT, BTN_B);
+	ioctl(fd, UI_SET_KEYBIT, BTN_X);
+	ioctl(fd, UI_SET_KEYBIT, BTN_Y);
+
+
+	/* This exits when the controller is upnplugged */
+	while 
